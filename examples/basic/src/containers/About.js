9e908e7828c78149916fe1147dfcde3cb374d38b
Tomas Balvin <tomas.balvin@cdn77.com>
2018-03-12 12:04:55 +0100

    replication of leading slash bug in About component

--- examples/basic/src/containers/About.js
+++ examples/basic/src/containers/About.js
@@ -1,10 +1,16 @@
 
 import React from 'react'
+import { withRouter } from 'react-static'
 //
 
-export default () => (
-  <div>
-    <h1>This is what we're all about.</h1>
-    <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
-  </div>
-)
+const about = ({ location: { pathname } }) => {
+  console.log(pathname)
+  return (
+    <div>
+      <h1>This is what we're all about.</h1>
+      <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
+    </div>
+  )
+}
+
+export default withRouter(about)
